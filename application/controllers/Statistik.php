021a4fb93d03b7e050009899086705cc2e9f087c
windys <windysabtami85@gmail.com>
2024-12-08 12:57:38 +0800

    perbaikkan sedikit

--- application/controllers/Statistik.php
+++ application/controllers/Statistik.php
@@ -1,20 +1,28 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Statistik extends CI_Controller {
+class Statistik extends CI_Controller
+{
 
-    public function index()
-    {
-        $this->load->model('M_Statistik_chart');
+	public function __construct()
+	{
+		parent::__construct();
+		// Set timezone
+		date_default_timezone_set('Asia/Jakarta');
+	}
 
-        $year = $this->input->get('year') ?: date('Y');
-        $month = $this->input->get('month') ?: date('m');
 
-        $data['chart_data'] = $this->M_Statistik_chart->get_chart_data($year, $month);
-        $this->load->view('template/new_header');
-        $this->load->view('template/new_sidebar');
-        $this->load->view('v_statistik', $data);
-        $this->load->view('template/new_footer');
-    }
+	public function index()
+	{
+		$this->load->model('M_Statistik_chart');
+
+		$year = $this->input->get('year') ?: date('Y');
+		$month = $this->input->get('month') ?: date('m');
+
+		$data['chart_data'] = $this->M_Statistik_chart->get_chart_data($year, $month);
+		$this->load->view('template/new_header');
+		$this->load->view('template/new_sidebar');
+		$this->load->view('v_statistik', $data);
+		$this->load->view('template/new_footer');
+	}
 }
-?>
