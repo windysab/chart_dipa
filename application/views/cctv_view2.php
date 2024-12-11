c4f4d60abf68cfbd8e4f56b44e9165689b268e63
windysab <windysabtami85@gmail.com>
2024-12-11 15:32:24 +0800

    oeke

--- application/views/cctv_view2.php
+++ application/views/cctv_view2.php
@@ -185,7 +185,7 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-6 chart-container">
+			<div class="col-md-6 table-container">
 				<h5>Chart Data Perkara Bulan <?php echo date('F Y', strtotime('first day of last month')); ?></h5>
 				<div class="card-body">
 					<div class="chart">
@@ -200,6 +200,14 @@
 						<canvas id="mediasiChart" style="min-height: 400px; height: 400px; max-height: 400px; max-width: 100%;"></canvas>
 					</div>
 				</div>
+			</div>
+		</div>
+		<div class="row text-center mt-4">
+			<div class="col-md-6">
+				<h5>Total Perkara Bulan <?php echo date('F Y', strtotime('first day of last month')); ?>: <?php echo htmlspecialchars($total_perkara_data->total_perkara, ENT_QUOTES, 'UTF-8'); ?></h5>
+			</div>
+			<div class="col-md-6">
+				<h5>Total Perkara Mediasi Bulan <?php echo date('F Y', strtotime('first day of last month')); ?>: <?php echo array_sum($mediasi_data); ?></h5>
 			</div>
 		</div>
 		<div class="row">
