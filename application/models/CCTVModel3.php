bf8c68f734c5cf469e52e0b9463fc91b285a89be
windys <windysabtami85@gmail.com>
2024-12-15 14:00:38 +0800

    oke

--- application/models/CCTVModel3.php
+++ application/models/CCTVModel3.php
@@ -122,29 +122,34 @@ class CCTVModel3 extends CI_Model
 		return $query->row()->sisa_perkara;
 	}
 
+
+
 	public function getSisaPerkaraBulanSebelumnya($year, $month)
 	{
+		// Menghitung bulan dan tahun sebelumnya
 		$previous_month = date('m', strtotime('1 month', strtotime("$year-$month-01")));
 		$previous_year = date('Y', strtotime('1 month', strtotime("$year-$month-01")));
-		
+
+		// Menjalankan query SQL untuk menghitung sisa perkara
 		$query = $this->db->query("
-            SELECT 
-                (SELECT COUNT(*) 
-                 FROM perkara
-                 LEFT JOIN perkara_efiling_id ON perkara.perkara_id = perkara_efiling_id.perkara_id
-                 LEFT JOIN perkara_efiling ON perkara_efiling_id.efiling_id = perkara_efiling.efiling_id
-                 LEFT JOIN perkara_putusan ON perkara.perkara_id = perkara_putusan.perkara_id
-                 WHERE YEAR(perkara.tanggal_pendaftaran) = ? AND MONTH(perkara.tanggal_pendaftaran) = ?) 
-                - 
-                (SELECT COUNT(*) 
-                 FROM perkara
-                 LEFT JOIN perkara_efiling_id ON perkara.perkara_id = perkara_efiling_id.perkara_id
-                 LEFT JOIN perkara_efiling ON perkara_efiling_id.efiling_id = perkara_efiling.efiling_id
-                 LEFT JOIN perkara_putusan ON perkara.perkara_id = perkara_putusan.perkara_id
-                 WHERE YEAR(perkara_putusan.tanggal_putusan) = ? AND MONTH(perkara_putusan.tanggal_putusan) = ?) 
-                AS sisa_perkara
-        ", [$previous_year, $previous_month, $previous_year, $previous_month]);
-		
+        SELECT 
+            (SELECT COUNT(*) 
+             FROM perkara
+             LEFT JOIN perkara_efiling_id ON perkara.perkara_id = perkara_efiling_id.perkara_id
+             LEFT JOIN perkara_efiling ON perkara_efiling_id.efiling_id = perkara_efiling.efiling_id
+             LEFT JOIN perkara_putusan ON perkara.perkara_id = perkara_putusan.perkara_id
+             WHERE YEAR(perkara.tanggal_pendaftaran) = ? AND MONTH(perkara.tanggal_pendaftaran) = ?) 
+            - 
+            (SELECT COUNT(*) 
+             FROM perkara
+             LEFT JOIN perkara_efiling_id ON perkara.perkara_id = perkara_efiling_id.perkara_id
+             LEFT JOIN perkara_efiling ON perkara_efiling_id.efiling_id = perkara_efiling.efiling_id
+             LEFT JOIN perkara_putusan ON perkara.perkara_id = perkara_putusan.perkara_id
+             WHERE YEAR(perkara_putusan.tanggal_putusan) = ? AND MONTH(perkara_putusan.tanggal_putusan) = ?) 
+            AS sisa_perkara
+    ", [$previous_year, $previous_month, $previous_year, $previous_month]);
+
+		// Mengembalikan hasil query sebagai jumlah sisa perkara
 		return $query->row()->sisa_perkara;
 	}
 }
