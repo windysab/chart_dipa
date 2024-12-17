fcca1161e081b1abb6a63b549ed457465f6ae19f
windysab <windysabtami85@gmail.com>
2024-12-17 12:08:52 +0800

    oke

--- application/views/cctv_view3.php
+++ application/views/cctv_view3.php
@@ -172,11 +172,11 @@
                 <div class="circle-card total-perkara-ecourt">
                     <p class="value"><?php echo htmlspecialchars($total_perkara_data->total_perkara_ecourt, ENT_QUOTES, 'UTF-8'); ?></p>
                 </div>
-                <p class="title">Total Perkara e-Court</p>
+                <p class="title">Perkara e-Court</p>
             </div>
             <div class="col-md-3">
                 <div class="circle-card persen-perkara-ecourt">
-                    <p class="value"><?php echo number_format($total_perkara_data->persen_perkara_ecourt, 2) . '%'; ?></p>
+                    <p class="value"><?php echo round($total_perkara_data->persen_perkara_ecourt) . '%'; ?></p>
                 </div>
                 <p class="title">Persentase Perkara e-Court</p>
             </div>
@@ -184,7 +184,7 @@
                 <div class="circle-card total-perkara-non-ecourt">
                     <p class="value"><?php echo htmlspecialchars($total_perkara_data->total_perkara_non_ecourt, ENT_QUOTES, 'UTF-8'); ?></p>
                 </div>
-                <p class="title">Total Perkara Non e-Court</p>
+                <p class="title">Perkara Non e-Court</p>
             </div>
         </div>
         <div class="row">
