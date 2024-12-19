6fbdfec6f17831c66a93f6ec525f99219bf2e19c
windysab <windysabtami85@gmail.com>
2024-12-19 15:02:12 +0800

    oke

--- application/config/database.php
+++ application/config/database.php
@@ -29,13 +29,40 @@ $query_builder = TRUE;
 
 
 
+// $db['default'] = array(
+// 	'dsn' => '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'u520364085_cctv',
+// 	'password' => 'Azka9285@Jihan',
+// 	'database' => 'u520364085_cctv',
+// 	// 'database' => 'data_stastik',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => TRUE,
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
+
+
 $db['default'] = array(
 	'dsn' => '',
 	'hostname' => 'localhost',
-	'username' => 'u520364085_cctv',
-	'password' => 'Azka9285@Jihan',
-	'database' => 'u520364085_cctv',
-	// 'database' => 'data_stastik',
+	// 'username' => 'u520364085_cctv',
+	// 'password' => 'Azka9285@Jihan',
+	// 'database' => 'u520364085_cctv',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'data_stastik',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
