fa051110e07913747aba5a0ef9cb19f7a4475e57
windysab <windysabtami85@gmail.com>
2024-12-19 14:39:39 +0800

    sds

--- application/views/cctv_view3.php
+++ application/views/cctv_view3.php
@@ -163,7 +163,7 @@
         </div>
         <div class="row">
             <div class="col-md-12 text-center mt-4">
-                <h2>Jumlah Tampilan: <?php echo isset($view_count) ? htmlspecialchars($view_count, ENT_QUOTES, 'UTF-8') : '0'; ?></h2>
+                <h6>Jumlah yang melihat: <?php echo isset($view_count) ? htmlspecialchars($view_count, ENT_QUOTES, 'UTF-8') : '0'; ?></h6>
             </div>
         </div>
         <div class="row total-perkara-container text-center">
