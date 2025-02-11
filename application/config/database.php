98fc38dd880e752af7c30104b326b3b0408eb4b4
windysab <windysabtami85@gmail.com>
2025-02-11 11:21:06 +0800

    oke

--- application/config/database.php
+++ application/config/database.php
@@ -1,15 +1,41 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => 'kadatahu123db',
+// 	'database' => 'sipp',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
+
+
 $db['default'] = array(
 	'dsn' => '',
-	'hostname' => '153.92.15.4',
-	'username' => 'u520364085_cctv', // Pastikan username benar
-	'password' => 'Azka9285@Jihan', // Pastikan password benar
-	'database' => 'u520364085_cctv', // Pastikan nama database benar
+	'hostname' => 'localhost',
+	'username' => 'u520364085_cctv',
+	'password' => 'Azka9285@Jihan',
+	'database' => 'u520364085_cctv',
+	// 'database' => 'data_stastik',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
