0be1740eb07261341e96b783181f0923e32223aa
windysab <windysabtami85@gmail.com>
2025-02-12 08:50:50 +0800

    fotgg

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,36 +1,11 @@
 <footer class="main-footer">
+    
+		<strong>Developed by <a href="https://github.com/windysab">Windy Sabtami, S.Kom</a> | 2025.</strong>
+	 
+		<div class="float-right d-none d-sm-inline-block">
+	 
+		
+	 </div>
+ </footer>
 
-	<strong>Developed by <a href="https://github.com/windysab">Windy Sabtami, S.Kom</a> | 2025.</strong>
-	<div class="row justify-content-center my-5 text-center">
-		<div id="histats_counter"></div>
-	</div>
-
-	<div class="float-right d-none d-sm-inline-block">
-
-
-	</div>
-
-	<!-- Histats.com  (div with counter) -->
-	<!-- Histats.com  START  (aync)-->
-	<script type="text/javascript">
-		var _Hasync = _Hasync || [];
-		_Hasync.push(['Histats.start', '1,4636066,4,2051,280,25,00011111']);
-		_Hasync.push(['Histats.fasi', '1']);
-		_Hasync.push(['Histats.track_hits', '']);
-		(function() {
-			var hs = document.createElement('script');
-			hs.type = 'text/javascript';
-			hs.async = true;
-			hs.src = ('//s10.histats.com/js15_as.js');
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-			hs.onload = function() {
-				console.log('Histats script loaded successfully');
-			};
-			hs.onerror = function() {
-				console.error('Error loading Histats script');
-			};
-		})();
-	</script>
-	<noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?4636066&101" alt="" border="0"></a></noscript>
-	<!-- Histats.com  END  -->
-</footer>
+ 
