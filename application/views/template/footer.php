043d3c326d1faa28d836151c8561dab8f15375d7
windysab <windysabtami85@gmail.com>
2025-02-12 08:33:08 +0800

    oke

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,8 +1,6 @@
   <footer class="main-footer">
-    <!-- <strong>Copyright &copy; 2014-2021 <a href="https://lupiananda.com">Lupi Ananda</a>.</strong>
-    All rights reserved.
-     -->
-     <strong>Developed by <a href="https://github.com/windysab">Windy Sabtami, S.Kom</a> | 2024.</strong>
+    
+     <strong>Developed by <a href="https://github.com/windysab">Windy Sabtami, S.Kom</a> | 2025.</strong>
     
      <div class="float-right d-none d-sm-inline-block">
     
