dbbcc6bb4218bbb10a4be708916fb9a51005d979
windys <windysabtami85@gmail.com>
2025-03-01 08:44:58 +0800

    sdsd

--- application/config/database.php
+++ application/config/database.php
@@ -29,12 +29,35 @@ $query_builder = TRUE;
 
 
 
+// $db['default'] = array(
+// 	'dsn' => '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'u520364085_cctv',
+// 	'password' => 'Azka9285@Jihan123',
+// 	'database' => 'u520364085_cctv',
+// 	// 'database' => 'data_stastik',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => TRUE,
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn' => '',
 	'hostname' => 'localhost',
-	'username' => 'u520364085_cctv',
-	'password' => 'Azka9285@Jihan123',
-	'database' => 'u520364085_cctv',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'sipp2025',
 	// 'database' => 'data_stastik',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
