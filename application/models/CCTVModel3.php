8e4bc252b18b2aee7b4c575f6d88cc7206cbdd3d
windysab <windysabtami85@gmail.com>
2025-04-08 16:03:48 +0800

    sss

--- application/models/CCTVModel3.php
+++ application/models/CCTVModel3.php
@@ -4,7 +4,7 @@ class CCTVModel3 extends CI_Model
 	private $links = [
         'Halaman Parkir' => 'https://paamuntai.cctvbadilag2.my.id/402556PAAMUNTAI/play.html?name=883576768194955166649178',
         'ruang_tunggu' => 'https://paamuntai.cctvbadilag2.my.id/402556PAAMUNTAI/play.html?name=303329746330290962284656',
-        'PTSP' => 'https://paamuntai.cctvbadilag2.my.id/402556PAAMUNTAI/play.html?name=846338227280923732711848'
+        'PTSP' => 'https://paamuntai.cctvbadilag2.my.id/402556PAAMUNTAI/play.html?name=514391471773000873180345'
     ];
 
 	public function getLinks()
