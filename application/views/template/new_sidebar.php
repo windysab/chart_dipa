ed3f0df775476103d04829ce764818f61ec0220f
windysab <windysabtami85@gmail.com>
2025-09-02 09:48:52 +0800

    agus

--- application/views/template/new_sidebar.php
+++ application/views/template/new_sidebar.php
@@ -214,7 +214,7 @@
 					</a>
 				</li>
 				<li class="nav-item">
-					<a href="<?php echo site_url('LKAgusPerkaraController') ?>" class="nav-link">
+					<a href="<?php echo site_url('LKAgustusPerkaraController') ?>" class="nav-link">
 						<i class="nav-icon fas fa-file-invoice-dollar"></i>
 						<p>
 							Laporan Keuangan Perkara Agustus
